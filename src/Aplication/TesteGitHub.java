619ba52ef6facc64b00215033f6675d9dac829ba
Matheus Bandeira <matheus281.1@hotmail.com>
2019-01-08 16:56:07 -0200

    Acrescentado uma nova aplicação

---
+++ src/Aplication/TesteGitHub.java
@@ -0,0 +1,9 @@
+package Aplication;
+
+public class TesteGitHub {
+
+	public static void main(String[] args) {
+		System.out.println("Escrevi e sai correndo, pau no cú de quem ta lendo kkkkkkkk");
+	}
+
+}
